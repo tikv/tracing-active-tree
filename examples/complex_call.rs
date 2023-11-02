82e9bcc92ea6398d1241ec058440d19715a320fb
hillium <yujuncen@pingcap.com>
2023-11-03 00:10:25 +0800

    initial commit
    Signed-off-by: hillium <yujuncen@pingcap.com>

---
+++ examples/complex_call.rs
@@ -0,0 +1,79 @@
+use std::time::Duration;
+
+use futures_util::future::BoxFuture;
+use tracing::instrument;
+use tracing_active_tree::{frame, layer, tree::formating::FormatFlat};
+use tracing_subscriber::prelude::*;
+
+/*
+This case is pretty like the example showed at the crate `tokio/async-backtrace`.
+
+Simple Output:
+6
+└[examples/complex_call.rs:41] [span_id=6] ["long_task"] [elapsed=316.137µs]
+ └[examples/complex_call.rs:43] [span_id=7] ["tokio::time::sleep(Duration::from_secs(40))"] [elapsed=291.971µs]
+1
+└[examples/complex_call.rs:23] [span_id=1] ["foo"] [elapsed=655.53µs]
+ └[examples/complex_call.rs:28] [span_id=2] ["bar"] [elapsed=575.366µs]
+  ├[examples/complex_call.rs:32] [span_id=3] ["futures::future::pending()"] [elapsed=539.468µs]
+  └[examples/complex_call.rs:36] [span_id=4] ["baz"] [elapsed=513.632µs]
+   └[examples/complex_call.rs:46] [span_id=5] ["quux"] [n=3] [elapsed=488.806µs]
+    ├[examples/complex_call.rs:58] [span_id=8] ["real_work"] [_id=0] [elapsed=228.006µs]
+    ├[examples/complex_call.rs:58] [span_id=9] ["real_work"] [_id=1] [elapsed=198.883µs]
+    └[examples/complex_call.rs:58] [span_id=10] ["real_work"] [_id=2] [elapsed=181.24µs]
+*/
+
+#[instrument]
+async fn foo() {
+    bar().await
+}
+
+#[instrument]
+async fn bar() {
+    tokio::select! {
+        _ = baz() => {}
+        _ = frame!(futures::future::pending()) => {}
+    }
+}
+
+#[instrument]
+async fn baz() {
+    quux(3).await
+}
+
+#[instrument]
+async fn long_task() {
+    frame!(tokio::time::sleep(Duration::from_secs(40))).await
+}
+
+#[instrument]
+async fn quux(n: usize) {
+    let tasks = (0..n).map(|n| Box::pin(real_work(n)) as BoxFuture<_>);
+    tokio::spawn(long_task());
+    // Give the newly spawned task a chance to be scheduled.
+    tokio::task::yield_now().await;
+    let print = async {
+        println!(
+            "{}",
+            String::from_utf8(
+                layer::global()
+                    .fmt_bytes_with(|tree, buf| tree.traverse_with(FormatFlat::new(buf)).unwrap())
+            )
+            .unwrap()
+        );
+    };
+    futures::future::join_all(tasks.chain(std::iter::once(Box::pin(print) as BoxFuture<_>))).await;
+}
+
+#[instrument]
+async fn real_work(_id: usize) {
+    tokio::task::yield_now().await;
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(layer::global().clone())
+        .init();
+    foo().await;
+}
---
+++ examples/wide_tree.rs
@@ -0,0 +1,65 @@
+use std::{cell::RefCell, collections::HashMap, time::Duration};
+
+use futures::channel::oneshot;
+use tracing::instrument;
+use tracing_active_tree::{frame, layer};
+use tracing_subscriber::prelude::*;
+
+/*
+
+This example shows how a wide active tree (probably some futures that waiting for many
+other futures concurrently) looks like.
+
+1
+└[examples/wide_tree.rs:13] [span_id=1] ["join_many"] [max=5] [elapsed=974.567µs]
+ ├[examples/wide_tree.rs:18] [span_id=3] ["nest_root"] [i=1] [elapsed=862.47µs]
+ │└[examples/wide_tree.rs:8] [span_id=2251799813685250] ["dummy"] [elapsed=606.968µs]
+ ├[examples/wide_tree.rs:18] [span_id=4] ["nest_root"] [i=2] [elapsed=867.766µs]
+ │├[examples/wide_tree.rs:8] [span_id=7] ["dummy"] [elapsed=576.435µs]
+ │└[examples/wide_tree.rs:8] [span_id=8] ["dummy"] [elapsed=539.281µs]
+ ├[examples/wide_tree.rs:18] [span_id=5] ["nest_root"] [i=3] [elapsed=861.4µs]
+ │├[examples/wide_tree.rs:8] [span_id=9] ["dummy"] [elapsed=516.472µs]
+ │├[examples/wide_tree.rs:8] [span_id=10] ["dummy"] [elapsed=497.408µs]
+ │└[examples/wide_tree.rs:8] [span_id=11] ["dummy"] [elapsed=475.498µs]
+ └[examples/wide_tree.rs:18] [span_id=6] ["nest_root"] [i=4] [elapsed=873.938µs]
+  ├[examples/wide_tree.rs:8] [span_id=12] ["dummy"] [elapsed=453.066µs]
+  ├[examples/wide_tree.rs:8] [span_id=13] ["dummy"] [elapsed=421.089µs]
+  ├[examples/wide_tree.rs:8] [span_id=14] ["dummy"] [elapsed=318.863µs]
+  └[examples/wide_tree.rs:8] [span_id=15] ["dummy"] [elapsed=295.081µs]
+ */
+
+#[instrument(skip_all)]
+async fn dummy(rx: oneshot::Receiver<()>) {
+    let _ = rx.await;
+}
+
+#[instrument]
+async fn join_many(max: usize) {
+    let v = RefCell::new(vec![]);
+    let futs = (0..max)
+        .map(|i| {
+            frame!("nest_root"; futures::future::join_all( (0..i).map(|_| {
+                    let (tx, rx) = oneshot::channel();
+                    v.borrow_mut().push(tx);
+                    dummy(rx)
+                })); i)
+        })
+        .collect::<Vec<_>>();
+    let (tx, rx) = oneshot::channel();
+    tokio::spawn(async move {
+        tokio::task::yield_now().await;
+        println!("{}", layer::global().fmt_string());
+        tx.send(()).unwrap();
+        drop(v);
+    });
+    futures::future::join_all(futs).await;
+    rx.await.unwrap()
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(layer::global().clone())
+        .init();
+    join_many(5).await;
+}
